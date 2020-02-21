12304c209f4ae1d21e1230de2e40491f976ed9ae
faber.v <faber.v@helix.ru>
2020-02-21 15:40:29 +0300

    added GetEnvVariable method

--- Config.cs
+++ Config.cs
@@ -7,9 +7,17 @@ namespace DaBot
 {
     public static class Config
     {
-        public static readonly string AccessToken = Environment.GetEnvironmentVariable("ACCESS_TOKEN", EnvironmentVariableTarget.User);
-        public static readonly string SecretKey = Environment.GetEnvironmentVariable("SECRET_KEY", EnvironmentVariableTarget.User);
-        public static readonly string Confirmation = Environment.GetEnvironmentVariable("CONFIRMATION", EnvironmentVariableTarget.User);
-        public static readonly long ConfirmationGroupId = long.Parse(Environment.GetEnvironmentVariable("CONFIRMATION_GROUP_ID", EnvironmentVariableTarget.User));
+        public static readonly string AccessToken = GetEnvVariable("ACCESS_TOKEN");
+        public static readonly string SecretKey = GetEnvVariable("SECRET_KEY");
+        public static readonly string Confirmation = GetEnvVariable("CONFIRMATION");
+        public static readonly long ConfirmationGroupId = long.Parse(GetEnvVariable("CONFIRMATION_GROUP_ID"));
+        
+        public static string GetEnvVariable(string envName)
+        {
+            return Environment.GetEnvironmentVariable(envName, EnvironmentVariableTarget.User) ?? 
+                   Environment.GetEnvironmentVariable(envName, EnvironmentVariableTarget.Process) ??
+                   Environment.GetEnvironmentVariable(envName, EnvironmentVariableTarget.Machine);
+        }
     }
+    
 }
