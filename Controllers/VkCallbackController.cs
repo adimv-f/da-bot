5ca76a22c9f07629c8fd47ba17cf3c2455e73bb5
faber.v <faber.v@helix.ru>
2020-02-21 10:33:44 +0300

    added message_new reaction

--- Controllers/VkCallbackController.cs
+++ Controllers/VkCallbackController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,9 +26,23 @@ namespace DaBot.Controllers
         [HttpPost]
         public IActionResult ConfirmApi([FromBody]UpdateDTO request)
         {
-            if (request.Type == "confirmation" && request.GroupId == Config.ConfirmationGroupId)
-                return Ok(Config.Confirmation);
-
+            switch (request.Type)
+            {
+                case "confirmation":
+                    if (request.GroupId == Config.ConfirmationGroupId)
+                        return Ok(Config.Confirmation);
+                    break;
+                
+                case "message_new":
+                    _vkApi.Messages.Send(new MessagesSendParams
+                    {
+                        PeerId = long.Parse(request.Object.RootElement.GetProperty("peer_id").ToString()),
+                        Message = "понял принял",
+                        RandomId = new Random().Next()
+                    });
+                    break;
+            }
+            
             return Ok("ok");
         }
     }
